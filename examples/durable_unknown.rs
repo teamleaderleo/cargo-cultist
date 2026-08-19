9024e98ead6b926ed573785827ee5a057de6db0c
Leo <cheerleaderleo@outlook.com>
2026-08-19 18:37:36 +0800

    test: format durable unknown example

--- examples/durable_unknown.rs
+++ examples/durable_unknown.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code)]
+
 use std::error::Error;
 use std::io::{self, Read};
 
@@ -5,10 +7,10 @@ use serde::Deserialize;
 
 #[path = "../src/applicability.rs"]
 mod applicability;
-#[path = "../src/justification.rs"]
-mod justification;
 #[path = "../src/durable_obligation.rs"]
 mod durable_obligation;
+#[path = "../src/justification.rs"]
+mod justification;
 
 use applicability::EvaluationContext;
 use durable_obligation::{ClearingEvidenceReceipt, DurableObligation, evaluate_obligation};
@@ -34,11 +36,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
 
     let request: EvaluationInput = serde_json::from_slice(&input)?;
-    let evaluation = evaluate_obligation(
-        &request.obligation,
-        &request.receipts,
-        &request.context,
-    )?;
+    let evaluation = evaluate_obligation(&request.obligation, &request.receipts, &request.context)?;
     serde_json::to_writer_pretty(io::stdout().lock(), &evaluation)?;
     println!();
     Ok(())

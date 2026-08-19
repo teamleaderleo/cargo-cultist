fd37861d73e966ed64538c60cc00bf32f8f928b2
Leo <cheerleaderleo@outlook.com>
2026-08-19 18:37:43 +0800

    test: format durable obligation harness

--- tests/durable_obligation.rs
+++ tests/durable_obligation.rs
@@ -2,7 +2,7 @@
 
 #[path = "../src/applicability.rs"]
 mod applicability;
-#[path = "../src/justification.rs"]
-mod justification;
 #[path = "../src/durable_obligation.rs"]
 mod durable_obligation;
+#[path = "../src/justification.rs"]
+mod justification;

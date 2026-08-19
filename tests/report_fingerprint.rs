89d68f6ffd18bbae09233eec461b9e0a541ab21a
Leo <cheerleaderleo@outlook.com>
2026-08-19 17:59:04 +0800

    test: run report fingerprint controls in standard suite

---
+++ tests/report_fingerprint.rs
@@ -0,0 +1,8 @@
+#![allow(dead_code)]
+
+#[path = "../src/finding.rs"]
+mod finding;
+#[path = "../src/compact_ir.rs"]
+mod compact_ir;
+#[path = "../src/report_fingerprint.rs"]
+mod report_fingerprint;

9113b2cc4157dea09ad5fe1ee62e3c9a47e3d3f0
Leo <cheerleaderleo@outlook.com>
2026-08-19 18:00:56 +0800

    style: apply rustfmt to fingerprint test harness

--- tests/report_fingerprint.rs
+++ tests/report_fingerprint.rs
@@ -1,8 +1,8 @@
 #![allow(dead_code)]
 
-#[path = "../src/finding.rs"]
-mod finding;
 #[path = "../src/compact_ir.rs"]
 mod compact_ir;
+#[path = "../src/finding.rs"]
+mod finding;
 #[path = "../src/report_fingerprint.rs"]
 mod report_fingerprint;
